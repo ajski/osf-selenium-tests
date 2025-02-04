ebe69fe89bbf70edb9158f43dc43843379708aff
Shtohryn <pavlo.stogryn@gmail.com>
2025-02-05 01:18:30 +0200

    Fix test_institution_admin_dashboard

--- pages/institutions.py
+++ pages/institutions.py
@@ -1,4 +1,7 @@
+import ipdb
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 import settings
 from base.locators import (
@@ -58,19 +61,15 @@ class InstitutionAdminDashboardPage(BaseInstitutionPage):
 
     url_addition = '/dashboard'
 
-    identity = Locator(By.CSS_SELECTOR, 'div[data-analytics-scope="Dashboard"]')
-    loading_indicator = Locator(By.CSS_SELECTOR, '.ball-scale', settings.LONG_TIMEOUT)
-    departments_listbox_trigger = Locator(
+    identity = Locator(By.CSS_SELECTOR, '._institution-banner_kv000k')
+    loading_indicator = Locator(By.CSS_SELECTOR, '.ball-scale ball-dark _LoadingIndicator_k6n1c2', settings.LONG_TIMEOUT)
+    title_containers = GroupLocator(
         By.CSS_SELECTOR,
-        'div.ember-basic-dropdown-trigger.ember-power-select-trigger._select_tdvp4z',
+        '._title-container_1d9vmx',
     )
-    total_user_count = Locator(
+    total_project_count = GroupLocator(
         By.CSS_SELECTOR,
-        'div.ember-view._panel_1dj7yu._sso-users-connected_1w5vdt > div > div._panel-body_1lht4i > div > h3',
-    )
-    total_project_count = Locator(
-        By.CSS_SELECTOR,
-        'div.ember-view._panel_1dj7yu._projects_1w5vdt > div > div._panel-body_1lht4i > div > div > h3',
+        '._kpi-container_1ge2xx',
     )
     public_project_count = Locator(
         By.CSS_SELECTOR, 'div._projects-count_1ky9tx > span:nth-child(1) > strong'
@@ -78,9 +77,8 @@ class InstitutionAdminDashboardPage(BaseInstitutionPage):
     private_project_count = Locator(
         By.CSS_SELECTOR, 'div._projects-count_1ky9tx > span:nth-child(2) > strong'
     )
-
     department_options = GroupLocator(
-        By.CSS_SELECTOR, 'ul.ember-power-select-options > li.ember-power-select-option'
+        By.CSS_SELECTOR, 'ul._data-list_1d9vmx > li._data-container_1d9vmx'
     )
     user_table_rows = GroupLocator(
         By.CSS_SELECTOR,
@@ -88,7 +86,27 @@ class InstitutionAdminDashboardPage(BaseInstitutionPage):
     )
 
     def select_department_from_listbox(self, department):
-        for option in self.department_options:
-            if option.text == department:
-                option.click()
-                break
+        for element in  self.department_options:
+            name_elem = element.find_element(By.CSS_SELECTOR, "div._name_1d9vmx")
+            if name_elem.text.strip() ==  department:
+                total_elem = element.find_element(By.CSS_SELECTOR, "div._total_1d9vmx")
+                return int(total_elem.text.strip())
+
+    def get_total_count_by_kpi_title(self, target_title):
+        for container in self.total_project_count:
+            title_element = container.find_element(By.CSS_SELECTOR, "._title_1ge2xx")
+            if title_element.text.strip() == target_title:
+                value_element = container.find_element(By.CSS_SELECTOR, "._total-container_1ge2xx")
+                return value_element.text.strip()
+
+    def click_on_listbox_trigger(self, section_title):
+        for section in self.title_containers:
+            title_element = section.find_element(By.CSS_SELECTOR, "._title_1d9vmx")
+            if title_element.text.strip() == section_title:
+                button = section.find_element(By.CSS_SELECTOR, "button._Button_6kisxq._FakeLink_6kisxq")
+                icon = section.find_element(By.CSS_SELECTOR, "[data-test-toggle-icon]")
+                button.click()
+                WebDriverWait(self.driver, 10).until(
+                    lambda d: icon.get_attribute("data-icon") == "caret-up"
+                )
+
